aed61f8b622817e42ad636f51ac7f6fb1c096c42
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-07-02 13:50:13 +0530

    ATM

---
+++ Main.java
@@ -0,0 +1,13 @@
+package atmproject;
+
+import java.io.IOException;
+
+public class Main extends OptionMenu {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+      OptionMenu optionMenu = new OptionMenu();
+      optionMenu.getLogin();
+	}
+
+}
